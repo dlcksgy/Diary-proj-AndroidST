2d07c9676eaa6823f8be65cc4d1b04c7cd31da69
dlcskgy <hyo517@naver.com>
2017-05-28 01:32:00 +0900

    종료버튼과 일기장 메뉴 구현

--- app/src/main/java/com/apress/gerber/diary_proj/MainActivity.java
+++ app/src/main/java/com/apress/gerber/diary_proj/MainActivity.java
@@ -1,56 +1,71 @@
 package com.apress.gerber.diary_proj;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private String[] sort = {"일기", "그림일기", "교환일기", "사진일기"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button listButton = (Button) findViewById(R.id.listButton);
+        listButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("메뉴");
+                builder.setItems(sort, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int i) {
+                        Toast.makeText(getApplicationContext(), sort[i], Toast.LENGTH_SHORT).show();
+                    }
+                });
+                AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+            }
+        });
+
+        Button exitButton = (Button) findViewById(R.id.exitButton);
+        exitButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setMessage("정말로 종료하시겠습니까?");
+                builder.setTitle("종료알림창")
+                        .setCancelable(false)
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                finish();
+                            }
+                        })
+                        .setNegativeButton("no", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.cancel();
+                            }
+                        });
+                AlertDialog alert = builder.create();
+                alert.setTitle("종료알림창");
+                alert.show();
+
+            }
+        });
+
+
+
     }
-    public void addClic(View v){
-        EditText number1 = (EditText) findViewById(R.id.number1);
-        EditText number2 = (EditText) findViewById(R.id.number2);
-        TextView result = (TextView) findViewById(R.id.result);
-        int n1 = Integer.parseInt(number1.getText().toString());
-        int n2 = Integer.parseInt(number2.getText().toString());
-        result.setText(Integer.toString(n1 + n2));
-    }
 
 
 
-    public void subtractClic(View v){
-        EditText number1 = (EditText) findViewById(R.id.number1);
-        EditText number2 = (EditText) findViewById(R.id.number2);
-        TextView result = (TextView) findViewById(R.id.result);
-        int n1 = Integer.parseInt(number1.getText().toString());
-        int n2 = Integer.parseInt(number2.getText().toString());
-        result.setText(Integer.toString(n1 - n2));
-    }
-
-
-    public void multiplyClic(View v){
-        EditText number1 = (EditText) findViewById(R.id.number1);
-        EditText number2 = (EditText) findViewById(R.id.number2);
-        TextView result = (TextView) findViewById(R.id.result);
-        int n1 = Integer.parseInt(number1.getText().toString());
-        int n2 = Integer.parseInt(number2.getText().toString());
-        result.setText(Integer.toString(n1 * n2));
-    }
-
-
-
-    public void divideClic(View v){
-        EditText number1 = (EditText) findViewById(R.id.number1);
-        EditText number2 = (EditText) findViewById(R.id.number2);
-        TextView result = (TextView) findViewById(R.id.result);
-        int n1 = Integer.parseInt(number1.getText().toString());
-        int n2 = Integer.parseInt(number2.getText().toString());
-        result.setText(Integer.toString(n1 / n2));
-    }
 }
